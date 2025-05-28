791ad095188cf9210198d1cd4bf7cdd1f4900a85
Ivo Oliveira <ivo793@gmail.com>
2025-05-27 22:19:56 -0300

    creating home and consuming api with axios

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,44 @@
+import { useState, useEffect } from "react";
+
+import { Link } from "react-router-dom";
+
+import './Home.scss'
+
+import axios from "axios";
 
 const Home = () =>{
-    return(
-        <div>
-            <h1>home</h1>
-        </div> 
+    const [posts, setPosts] = useState([])
+
+    const getPosts = async () =>{
+        try{
+            const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+            
+            const data = res.data
+
+            setPosts(data)
+        }catch(err){
+            throw new Error(err)
+        }
+    }
+
+    useEffect(() =>{
+        getPosts()
+    },[])
+
+    return (
+        <div className="home">
+            <h1>All Posts</h1>
+            {posts.length === 0 ? <p>Loading</p> : (
+                posts.map((post) => (
+                    <div className="post" key={post.id}>
+                        <h2>{post.title}</h2>
+                        <p>{post.body}</p>
+                        <Link to={`/posts/${post.id}`} className="btn">Read more</Link>
+                    </div>
+                ))
+            )}
+        </div>
+       
     )
    
 }
